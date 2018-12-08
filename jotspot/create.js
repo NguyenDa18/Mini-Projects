f3da105e6c5c84f7c59e9bc4699c097d0307c419
Danh Nguyen <danhnguyen267@gmail.com>
2018-12-08 05:55:54 +0000

    Init ServerlessStack project -create Lambda added

---
+++ jotspot/create.js
@@ -0,0 +1,48 @@
+import uuid from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+    // Req body passed in as JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+    
+    const params = {
+        TableName: "notes",
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId, // user identity id of Cognito Identity Pool as user id of authenticated user
+            noteId: uuid.v1(), // unique id
+            content: data.content, // parsed from req body
+            attachment: data.attachment, // parsed from req body
+            createdAt: Date.now() // current Unix timestamp
+        }
+    };
+    
+    dynamoDb.put(params, (err, data) => {
+        // Set response headers to enable CORS
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
+        
+        // return status code 500 on err
+        if (err) {
+            const response = {
+                statusCode: 500,
+                headers: headers,
+                body: JSON.stringify({ status: false })
+            };
+            callback(null, response);
+            return;
+        }
+        
+        // return status code 200 and newly created item on success
+        const response = {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify(params.Item)
+        };
+        callback(null, response);
+    });
+    
+}
